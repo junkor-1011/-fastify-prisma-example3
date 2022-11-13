ea3afa9bc11c49d8f8b5304e52cec25b02cc1bb9
junkor-1011 <tomoyukizxy@gmail.com>
2022-11-13 19:11:35 +0900

    fasitfy simple app

---
+++ src/app.ts
@@ -0,0 +1,22 @@
+/* eslint-disable @typescript-eslint/no-floating-promises */
+
+import fastify from 'fastify';
+
+export const server = fastify({
+  logger: true,
+});
+
+server.get('/', async (request, reply) => {
+  return { hello: 'world' };
+});
+
+const main = async (): Promise<void> => {
+  try {
+    await server.listen({ port: 3000, host: '0.0.0.0' });
+    console.log('Server listening on port 3000.');
+  } catch (err) {
+    console.log(err);
+    process.exit(1);
+  }
+};
+main();
