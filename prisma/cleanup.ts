8e4e5cc5cb2fc8b19d32ae11141a151cbd119d97
junkor-1011 <tomoyukizxy@gmail.com>
2023-01-17 00:09:53 +0900

    feat: cleanup script

---
+++ prisma/cleanup.ts
@@ -0,0 +1,21 @@
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+async function main(): Promise<void> {
+  console.log('Start cleanup db records...')
+  await prisma.$transaction([
+    prisma.user.deleteMany(),
+  ])
+  console.log('Finished cleanup db records...')
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
