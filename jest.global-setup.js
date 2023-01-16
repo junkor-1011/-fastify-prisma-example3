56bc9925ed7a36d3d31982e4b6f27ae6b95c810f
junkor-1011 <tomoyukizxy@gmail.com>
2023-01-17 00:51:54 +0900

    testing: integration testing example

--- jest.global-setup.js
+++ jest.global-setup.js
@@ -1,4 +1,5 @@
 module.exports = async () => {
   process.env.TZ = 'Asia/Tokyo';
   process.env.LANG = 'ja_JP.UTF-8';
+  process.env.DATABASE_URL = 'postgresql://pguser:password@localhost:5432/appdb?schema=public';
 };
--- test/integration/api-user.test.ts
+++ test/integration/api-user.test.ts
@@ -1,5 +1,54 @@
-describe('integration test', () => {
-  it('empty test', async () => {
-    expect(true).toBe(true)
+import { buildApp } from '@/app'
+import { prisma } from '@/libs/prisma'
+
+const userTestData = [
+  {
+    "id": "b779545c-6c21-47a4-b087-e70985e57af1",
+    "name": "Alice",
+    "email": "alice@example.com",
+    "rank": 1,
+    "birthdate": "2022-12-04T13:07:58.786Z",
+    "createdAt": "2022-12-04T13:08:18.653Z",
+    "updatedAt": "2022-12-04T13:08:18.653Z"
+  },
+  {
+    "id": "b779545c-6c21-47a4-b087-e70985e57af2",
+    "name": "Bob",
+    "email": "bob@example.com",
+    "rank": 3,
+    "birthdate": "2022-11-04T13:07:58.000Z",
+    "createdAt": "2022-11-04T13:08:18.000Z",
+    "updatedAt": "2022-11-04T13:08:18.000Z"
+  },
+  {
+    "id": "a779545c-6c21-47a4-b087-e70985e57aff",
+    "name": "富士山",
+    "email": "hoge@example.com",
+    "rank": 1,
+    "birthdate": "2021-12-04T13:07:58.786Z",
+    "createdAt": "2021-12-04T13:08:18.653Z",
+    "updatedAt": "2021-12-04T13:08:18.653Z"
+  }
+];
+
+beforeEach(async() => {
+  jest.useRealTimers(); // to avoid fastify.inject stopping
+  await prisma.user.deleteMany();
+  await prisma.user.createMany({ data: userTestData });
+}, 5000)
+
+afterEach(async() => {
+  await prisma.user.deleteMany();
+}, 5000)
+
+describe('GET /users', () => {
+  it('get records', async () => {
+    jest.useRealTimers();
+    const app = await buildApp();
+    const response = await app.inject('/users')
+    // smoke test
+    expect(response.statusCode).toBe(200)
+    // check record
+    expect(response.json()).toEqual(userTestData)
   })
 })
